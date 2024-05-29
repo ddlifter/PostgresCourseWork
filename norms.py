9cf65502bf35a23f143525b5b5e2d87045881c25
ddlifter <butorin_kirya@inbox.ru>
2024-05-29 14:23:41 +0300

    feat(norms): Implement updating

--- norms.py
+++ norms.py
@@ -3,18 +3,19 @@ import psycopg2
 
 from connection import ConnectionManager
 from add_norm import AddNorm
+from update_norm import UpdateNorm  # Подключаем класс для окна обновления сотрудника
 
 class Norms(QWidget):
-    def __init__(self, conn: ConnectionManager, IsAdmin) :
-        self.conn : ConnectionManager = conn
+    def __init__(self, conn: ConnectionManager, IsAdmin):
         super().__init__()
+        self.conn: ConnectionManager = conn
         self.showMaximized()
         layout = QVBoxLayout()
         self.setWindowTitle("Нормы")
         self.setGeometry(100, 100, 600, 400)
         self.setLayout(layout)
         self.table_widget = QTableWidget()
-        self.table_widget.setGeometry(50, 50, 500, 300)  # Установите размеры и позицию
+        self.table_widget.setGeometry(50, 50, 500, 300)
         
         self.add_button = QPushButton("Добавить")
         self.add_button.clicked.connect(self.open_add_dialog)
@@ -24,45 +25,81 @@ class Norms(QWidget):
         self.show_data_button.clicked.connect(self.load_data_from_db)
         layout.addWidget(self.show_data_button)
         
+        self.update_button = QPushButton("Обновить")
+        self.update_button.clicked.connect(self.open_update_dialog)
+        layout.addWidget(self.update_button)
+        
         self.delete_button = QPushButton("Удалить")
         self.delete_button.clicked.connect(self.delete_selected_row)
         layout.addWidget(self.delete_button)
         
         layout.addWidget(self.table_widget)
-        
+
         if IsAdmin != True:
             self.add_button.setEnabled(False)
             self.delete_button.setEnabled(False)
+        
 
     def load_data_from_db(self):
+        query = """
+            SELECT norms.name, norms.description, ranks.name AS rank, ranks.description
+            FROM norms
+            INNER JOIN ranks ON norms.id_rank = ranks.id_rank
+        """
         with self.conn as conn:
-            with conn.cursor() as cursor:
-                cursor.execute("SELECT * FROM norms")  # Убедитесь, что имя таблицы верно
-                rows = cursor.fetchall()
-                self.table_widget.setRowCount(len(rows))
-                self.table_widget.setColumnCount(len(rows[0]))
-                for i, row in enumerate(rows):
-                    for j, value in enumerate(row):
-                        self.table_widget.setItem(i, j, QTableWidgetItem(str(value)))
+            with conn.cursor() as cur:
+                cur.execute(query)
+                rows = cur.fetchall()
+
+        self.table_widget.setRowCount(len(rows))
+        self.table_widget.setColumnCount(len(rows[0]))
+
+        for i, row in enumerate(rows):
+            for j, value in enumerate(row):
+                self.table_widget.setItem(i, j, QTableWidgetItem(str(value)))
+
 
     def open_add_dialog(self):
         dialog = AddNorm(self.conn)
         if dialog.exec_():
             self.load_data_from_db()
             
+    
     def delete_selected_row(self):
         with self.conn as conn:
             with conn.cursor() as cursor:
-                selected_rows = self.table_widget.selectionModel().selectedRows()
-                if not selected_rows:
-                    QMessageBox.information(self, "Уведомление", "Выберите строку для удаления.")
+                selected_indexes = self.table_widget.selectionModel().selectedRows()
+                if not selected_indexes:
+                    QMessageBox.information(self, "Уведомление", "Выебрите строку для удаления.")
                     return
-                norm_id = self.table_widget.item(selected_rows[0].row(), 0).text()  # Предполагается, что ID сотрудника находится в первом столбце
+
+                index = selected_indexes[0]
+                surname = self.table_widget.item(index.row(), 0).text()
+                name = self.table_widget.item(index.row(), 1).text()
                 try:
-                    cursor.execute("DELETE FROM norms WHERE id_norm = %s", (norm_id,))
+                    cursor.execute("DELETE FROM norms WHERE name = %s AND description = %s", (surname, name))
                     conn.commit()
                     QMessageBox.information(self, "Успех", "Строка успешно удалена.")
                     self.load_data_from_db()
                 except Exception as e:
-                    QMessageBox.critical(self, "Ошибка", f"Не удалось удалить строку: {str(e)}")
+                    QMessageBox.critical(self, "Error", f"Failed to delete row: {str(e)}")
 
+                    
+    def open_update_dialog(self):
+        selected_indexes = self.table_widget.selectionModel().selectedRows()
+        if not selected_indexes:
+            QMessageBox.information(self, "Reminder", "Select a row to update.")
+            return
+
+        selected_row_index = selected_indexes[0].row()
+        surname = self.table_widget.item(selected_row_index, 0).text()
+        name = self.table_widget.item(selected_row_index, 1).text()
+        
+        with self.conn as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT id_norm FROM norms WHERE name = %s AND description = %s", (surname, name))
+                id_employee = cur.fetchone()[0]
+
+        dialog = UpdateNorm(self.conn, id_employee, surname, name)
+        if dialog.exec_():
+            self.load_data_from_db()
---
+++ update_norm.py
@@ -0,0 +1,69 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLineEdit, QLabel, QDialog, QMessageBox, QComboBox
+import psycopg2
+from connection import ConnectionManager
+
+class UpdateNorm(QDialog):
+    def __init__(self, conn: ConnectionManager, id_employee, surname, name):
+        super().__init__()
+        self.conn: ConnectionManager = conn
+        self.id_employee = id_employee
+
+        self.setWindowTitle("Обновление информации о норме")
+        layout = QVBoxLayout()
+
+        self.surname_label = QLabel("Название:")
+        self.surname_input = QLineEdit()
+        self.surname_input.setText(surname)
+        layout.addWidget(self.surname_label)
+        layout.addWidget(self.surname_input)
+
+        self.name_label = QLabel("Описание:")
+        self.name_input = QLineEdit()
+        self.name_input.setText(name)
+        layout.addWidget(self.name_label)
+        layout.addWidget(self.name_input)
+
+        self.specialty_label = QLabel("Разряд:")
+        self.specialty_combo = QComboBox()
+        layout.addWidget(self.specialty_label)
+        layout.addWidget(self.specialty_combo)
+
+        self.submit_button = QPushButton("Подтвердить")
+        self.submit_button.clicked.connect(self.submit_data)
+        layout.addWidget(self.submit_button)
+
+        self.setLayout(layout)
+
+        self.load_specialties()
+
+    def load_specialties(self):
+        with self.conn as conn:
+            with conn.cursor() as cur:
+                try:
+                    cur.execute("SELECT id_rank, name FROM ranks")
+                    specialties = cur.fetchall()
+                    for specialty_id, specialty_name in specialties:
+                        self.specialty_combo.addItem(specialty_name, specialty_id)
+                except Exception as e:
+                    QMessageBox.critical(self, "Ошибка", f"Не удалось загрузить специальности: {str(e)}")
+
+    def submit_data(self):
+        new_surname = self.surname_input.text()
+        new_name = self.name_input.text()
+        new_specialty_id = self.specialty_combo.currentData()
+
+        if not new_surname or not new_name:
+            QMessageBox.critical(self, "Ошибка", "Фамилия и имя не могут быть пустыми.")
+            return
+
+        with self.conn as conn:
+            with conn.cursor() as cur:
+                try:
+                    cur.execute("UPDATE norms SET name = %s, description = %s, id_rank = %s WHERE id_norm = %s",
+                                (new_surname, new_name, new_specialty_id, self.id_employee))
+                    conn.commit()
+                    QMessageBox.information(self, "Успех", "Информация о сотруднике успешно обновлена.")
+                    self.accept()  # Закрыть диалоговое окно
+                except Exception as e:
+                    QMessageBox.critical(self, "Ошибка", f"Не удалось обновить информацию о сотруднике: {str(e)}")
